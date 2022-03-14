8ec0ca399bb0572d35aa2f8f448bd2cd43d7359c
Anderson Silva <anderson.rs1996@outlook.com>
2022-03-14 15:09:56 -0300

    Add project files.

---
+++ CalculadoraPOO.ConsoleApp/Operações.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace CalculadoraPOO.ConsoleApp
+{
+    public class Operações
+    {
+       
+        public Resultado[] resultados;
+
+       
+        public void Somar()
+        {
+            Console.WriteLine("\n--------------------\n");
+            Console.WriteLine("ADIÇÃO");
+            Console.WriteLine("Primeiro numero: ");
+            double primeiroNumeroSoma = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Segundo numero: ");
+            double segundoNumeroSoma = Convert.ToInt32(Console.ReadLine());
+            double resultado = primeiroNumeroSoma + segundoNumeroSoma;
+            Console.WriteLine("O resultado da adição é " + resultado);
+            Console.WriteLine("\n--------------------\n");
+            for (int i = 0; i < this.resultados.Length; i++)
+            {
+                if (this.resultados[i] == null)
+                {
+                    Resultado resultadoSoma = new Resultado();
+                    resultadoSoma.resultadoOperacao= "A " + i + "º operação: " + primeiroNumeroSoma + " + " + segundoNumeroSoma + " = " + resultado + "\n";
+                    resultados[i] = resultadoSoma;
+                    break;
+                }
+            }
+        }
+
+
+        public void Subtrair()
+        {
+            Console.WriteLine("\n--------------------\n");
+            Console.WriteLine("SUBTRAÇÃO");
+            Console.WriteLine("Primeiro numero: ");
+            double primeiroNumeroSubtrair = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Segundo numero: ");
+            double segundoNumeroSubtrair = Convert.ToInt32(Console.ReadLine());
+            
+            double resultado = primeiroNumeroSubtrair - segundoNumeroSubtrair;
+            Console.WriteLine("O resultado da subtração é " + resultado);
+            Console.WriteLine("\n--------------------\n");
+            for (int i = 0; i < this.resultados.Length; i++)
+            {
+                if (this.resultados[i] == null)
+                {
+                    Resultado resultadoSoma = new Resultado();
+                    resultadoSoma.resultadoOperacao = "A " + i + "º operação: " + primeiroNumeroSubtrair + " - " + segundoNumeroSubtrair + " = " + resultado + "\n";
+                    resultados[i] = resultadoSoma;
+                    break;
+                }
+            }
+        }
+
+        public  void Divisão()
+        {
+            Console.WriteLine("\n--------------------\n");
+            Console.WriteLine("DIVISÃO");
+            Console.WriteLine("Primeiro numero: ");
+            double primeiroNumeroDividir = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Segundo numero: ");
+            double segundoNumeroDividir = Convert.ToInt32(Console.ReadLine());
+
+            if (segundoNumeroDividir != 0)
+            {
+                double resultado = ((double)primeiroNumeroDividir / segundoNumeroDividir);
+                Console.WriteLine("O resultado da Divisão é " + resultado);
+                Console.WriteLine("\n--------------------\n");
+                for (int i = 0; i < this.resultados.Length; i++)
+                {
+                    if (this.resultados[i] == null)
+                    {
+                        Resultado resultadoSoma = new Resultado();
+                        resultadoSoma.resultadoOperacao = "A " + i + "º operação: " + primeiroNumeroDividir + " / " + segundoNumeroDividir + " = " + resultado + "\n";
+                        resultados[i] = resultadoSoma;
+                        break;
+                    }
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Não é posivel divisão com o numero 0!!!");
+            }
+        }
+
+        public  void Multiplicar()
+        {
+            Console.WriteLine("\n--------------------\n");
+            Console.WriteLine("MULTPLICAÇÃO");
+            Console.WriteLine("Primeiro numero: ");
+            double primeiroNumeroMultiplicar = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Segundo numero: ");
+            double segundoNumeroMultiplicar = Convert.ToInt32(Console.ReadLine());
+
+            double resultado = primeiroNumeroMultiplicar * segundoNumeroMultiplicar;
+            Console.WriteLine("O resultado da multplicação é " + resultado);
+            Console.WriteLine("\n--------------------\n");
+            for (int i = 0; i < this.resultados.Length; i++)
+            {
+                if (this.resultados[i] == null)
+                {
+                    Resultado resultadoSoma = new Resultado();
+                    resultadoSoma.resultadoOperacao = "A " + i + "º operação: " + primeiroNumeroMultiplicar + " * " + segundoNumeroMultiplicar + " = " + resultado + "\n";
+                    resultados[i] = resultadoSoma;
+                    break;
+                }
+            }
+        }
+
+        public void RestoDivisão()
+        {
+            Console.WriteLine("\n--------------------\n");
+            Console.WriteLine("RESTO DA DIVISÃO");
+            Console.WriteLine("Primeiro numero: ");
+            double primeiroNumeroResto = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Segundo numero: ");
+            double segundoNumeroResto = Convert.ToInt32(Console.ReadLine());
+
+            if (segundoNumeroResto != 0)
+            {
+                double resultado = ((double)primeiroNumeroResto % segundoNumeroResto);
+                Console.WriteLine("O o resto da Divisão é " + resultado);
+                for (int i = 0; i < this.resultados.Length; i++)
+                {
+                    if (this.resultados[i] == null)
+                    {
+                        Resultado resultadoSoma = new Resultado();
+                        resultadoSoma.resultadoOperacao = "A " + (i+1) + "º operação: " + primeiroNumeroResto + " % " + segundoNumeroResto + " = o resto da divisão é " + resultado + "\n";
+                        resultados[i] = resultadoSoma;
+                        break;
+                    }
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Não é posivel divisão com o numero 0!!!");
+            }
+        }
+        
+       
+
+        
+    }
+}
---
+++ CalculadoraPOO.ConsoleApp/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CalculadoraPOO.ConsoleApp
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+
+            // ainda quero ver uma implementação do tamanhos dos objetos, para que seja "dinamica"
+            Operações operação = new Operações();
+            operação.resultados = new Resultado[100];
+            
+            string opcao = "";
+            while (opcao != "s")
+            {
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.Clear();
+                Console.WriteLine("Calculadora Topzera!!! 1.5");
+                Console.WriteLine("Digite 1 para somar ");
+                Console.WriteLine("Digite 2 para subtrair");
+                Console.WriteLine("Digite 3 para multiplicar");
+                Console.WriteLine("Digite 4 para dividir");
+                Console.WriteLine("Digite 5 para obter o resto da divisão");
+                Console.WriteLine("Digite 6 para obter os historicos");
+                Console.WriteLine("Digite 's' para fechar o progama");
+                opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+
+                    case "1":
+                       
+                        operação.Somar();
+                        break;
+
+                    case "2":
+                        
+                        operação.Subtrair();
+                        break;
+
+                    case "3":
+                        
+                        operação.Multiplicar();
+                        break;
+
+                    case "4":
+                        
+                        operação.Divisão();
+                        break;
+
+                    case "5":
+                        
+                        operação.RestoDivisão();
+                        break;
+
+                    case "6":
+                        Historico(operação);
+
+                        break;
+
+                    case "s":
+                        Console.WriteLine("FECHAR CALCULADORA\nATÉ A PROXIMA!!!");
+                        break;
+                   
+                    default:
+                        Console.WriteLine("OPÇÃO INVALIDA.\n DIGITE UMA DAS OPÇÕES SUGERIDAS NO MENU!!!");
+                        break;
+
+                }
+                
+                Console.ReadLine();
+            }
+            #region Resolução apenas com While
+            //.....................................OPÇÃO SEM SWITCH CASE..........................
+            //.....................................APENAS COM WHILE..............................
+
+
+            /*string opcao = "";
+            while(opcao != "s")
+            {
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.Clear();
+                Console.WriteLine("Calculadora Topzera!!! 1.5");
+                Console.WriteLine("Digite 1 para somar ");
+                Console.WriteLine("Digite 2 para subtrair");
+                Console.WriteLine("Digite 3 para multiplicar");
+                Console.WriteLine("Digite 4 para dividir");
+                Console.WriteLine("Digite 5 para obter o resto da divisão");
+                Console.WriteLine("Digite 's' para fechar o progama");
+                opcao = Console.ReadLine();
+                
+                Console.WriteLine("-------------------------");
+                if (opcao == "s")
+                    {
+                    break;
+                    }
+                else if ( opcao == "1" || opcao == "2" || opcao == "3" || opcao == "4" || opcao == "5")
+                { 
+
+                    Console.WriteLine("Primeiro numero: ");
+                    string strPrimeiroNumero = Console.ReadLine();
+                    Console.WriteLine("Segundo numero: ");
+                    string strSegundoNumero = Console.ReadLine();
+
+                   
+
+                    int primeiroNumero = Convert.ToInt32(strPrimeiroNumero);
+                    int segundoNumero = Convert.ToInt32(strSegundoNumero);
+                    if (opcao == "1")
+                    {
+                        int resultado = primeiroNumero + segundoNumero;
+                        Console.WriteLine("O resultado da adição é " + resultado);
+
+
+                    }
+                    else if (opcao == "2")
+                    {
+                        int resultado = primeiroNumero - segundoNumero;
+                        Console.WriteLine("O resultado do subtração é " + resultado);
+
+                    }
+                    else if (opcao == "3")
+                    {
+                        int resultado = primeiroNumero * segundoNumero;
+                        Console.WriteLine("O resultado da multiplicação é " + resultado);
+
+                    }
+                    else if (opcao == "4")
+                    {
+                        if (segundoNumero != 0)
+                        {
+                            float resultado = ((float)primeiroNumero / segundoNumero);
+                            Console.WriteLine("O resultado da Divisão é " + resultado);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Não é posivel divisão com o numero 0!!!");
+                        }
+                    }
+                    else if (opcao == "5")
+                    {
+                        if (segundoNumero != 0)
+                        {
+                            float resultado = ((float)primeiroNumero % segundoNumero);
+                            Console.WriteLine("O o resto da Divisão é " + resultado);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Não é posivel divisão com o numero 0!!!");
+                        }
+                    }
+
+                }
+
+                Console.WriteLine("-----------------------");
+                Console.ReadLine();            
+            }*/
+            #endregion
+        }
+
+        private static void Historico(Operações operação)
+        {
+            for (int i = 0; i < operação.resultados.Length; i++)
+            {
+
+                if (operação.resultados[i] != null)
+                {
+                    Console.WriteLine(operação.resultados[i].resultadoOperacao);
+                }
+            }
+        }
+    }
+}
+
+
